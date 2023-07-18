d08dcb87079655b5146a530cd2088aacb0cb7135
ANANDNJIT <aplv180@gmail.com>
2023-07-18 19:33:14 -0400

    update javascript file, add validate length function

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -3,8 +3,27 @@
 
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+var CodeLength = prompt(" Please,Select the desired length of the  password between the 8 and 128 characters! ,");
+
+
+if (CodeLength < 8 || CodeLength > 128 || isNaN(CodeLength) ) {
+
+  alert ("Your Password is invalid length. Please select your password as valid length( Between 8 and 128 characters)");
+  var CodeLength = prompt(" Please,Select the desired length of the  password between the 8 and 128 characters! ,");
+
+}
+
+
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
