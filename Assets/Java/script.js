72d66bb4c1647afe4e16de76056843b5c9c35478
ANANDNJIT <aplv180@gmail.com>
2023-07-19 16:14:16 -0400

    add comment on javascript file

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -1,24 +1,26 @@
-// Assignment code here
 
-
-
-
-
-
-// Get references to the #generate element
+/* Assign the variable */
 var generateBtn = document.querySelector("#generate");
 
+
+/* Generate the password  based on user input */
 function generatePassword() {
 
+
+/* Prompts the user to select the desired length:Between 8 and 128 characters  */
 var CodeLength = prompt(" Please,Select the desired length of the  password between the 8 and 128 characters! ,");
 
 
+/*  Set the conditional statement if user entered the invalid length.  */
 if (CodeLength < 8 || CodeLength > 128 || isNaN(CodeLength) ) {
+
 
   alert("Your Password is invalid length. Please select your password as valid length( Between 8 and 128 characters)");
   var CodeLength = prompt(" Please,Select the desired length of the  password between the 8 and 128 characters! ,");
 
 }
+
+/* Prompt user to confirm , include Lowercase, Uppercase, Numeric, Symbol */
 
 var AddLowerCase = confirm("Would you like Your Password as a (LowerCase) letter!");
 var AddUpperCase = confirm("Would you like Your Password as a (UpperCase) letter!");
@@ -26,19 +28,28 @@ var AddNumeric = confirm("Would you like Your Password as a (Numeric) Number!");
 var AddSymbol = confirm("Would you like Your Password as a (Special Symbol)!  Like,@,$,%, etc.");
  
 
+/* Set the characters to generate the password  */
 var IncludeLowerCase ="abcdefghijklmnopqrstuvwxyz";
 var IncludeUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var IncludeNumeric ="0123456789";
 var IncludeSymbol ="!@#$%^&*(){}:<>[]?" 
 
+
+/* Set the variable to store the generated password. */
 var characters = "";
 
-if (IncludeLowerCase == false && !IncludeUpperCase == false && !IncludeSymbol == false && !IncludeNumeric == false) {
-  alert( " Please,You need to select at least one type of characters! ")
-  return "";
-}
- 
 
+/*   Set the conditional statement if the user not selected any character type.   */
+if (!AddLowerCase && !AddUpperCase && !AddSymbol && !AddNumeric) {
+  alert("Please select at least one type of character.");
+  var AddLowerCase = confirm("Would you like Your Password as a (LowerCase) letter!");
+  var AddUpperCase = confirm("Would you like Your Password as a (UpperCase) letter!");
+var AddNumeric = confirm("Would you like Your Password as a (Numeric) Number!");
+var AddSymbol = confirm("Would you like Your Password as a (Special Symbol)!  Like,@,$,%, etc.");
+}
+
+
+/* Set the variable based on user selected  types*/
 if (AddUpperCase){
   characters += IncludeUpperCase ;
 }
@@ -57,7 +68,11 @@ if (AddSymbol){
   characters += IncludeSymbol ;
 }
 
+/* Set the variable to store the generated password.*/ 
+
 var password ="";
+
+/* Generate the random password using for loop.*/
 
 for ( let i = 0; i < CodeLength; i++) {
 
@@ -68,11 +83,8 @@ for ( let i = 0; i < CodeLength; i++) {
 return password;
 }
 
-
-
-
-
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Password Generator Starter Code
+
+
+# Ap_PassCode_Generator
+
+
+## Description 
+
+
+## Table of Contents
+
+
+- [Password Generator Starter Code](#password-generator-starter-code)
+- [Ap\_PassCode\_Generator](#ap_passcode_generator)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+
+## Installation
+
+   
+
+## Usage
+
+
+ 
+ 
+
+
+
+## Credits
+ 
+
+
+## License
+
